d7d857b9fcd9dd8855161e330b26894a75683a6b
Undying-yueyue <orgtech007@gmail.com>
2021-12-23 03:01:57 +0100

    mata: Build Gnss

--- device.mk
+++ device.mk
@@ -149,6 +149,10 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
     android.hardware.gatekeeper@1.0-service
 
+# GNSS
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@1.0.vendor
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.0-service.mata
