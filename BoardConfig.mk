243ff45522efc3a061bce5a3b5d9cfd560a5f058
mikeNG <mikeioannina@gmail.com>
2021-12-23 03:46:55 +0100

    mata: sepolicy: Switch to SYSTEM_EXT_{PUBLIC,PRIVATE}_SEPOLICY_DIRS
    Fixes:
    warning: BOARD_PLAT_PRIVATE_SEPOLICY_DIR has been deprecated.
        Use SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS instead.
    warning: BOARD_PLAT_PUBLIC_SEPOLICY_DIR has been deprecated.
        Use SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS instead.
    Change-Id: I752602079de8ff4c5370fe3ec861b8746838d878

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -130,8 +130,8 @@ VENDOR_SECURITY_PATCH := 2020-02-05
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 
 # Treble
 BOARD_VNDK_VERSION := current
