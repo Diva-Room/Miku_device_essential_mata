ed9af2005b7d34cbd8a3e81608e81832e48489d7
AmeChanRain <downloadbot007@gmail.com>
2022-09-28 10:34:08 +0000

    mata: Use proton clang

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -97,6 +97,8 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_SOURCE := kernel/essential/msm8998
 TARGET_KERNEL_CONFIG := lineageos_mata_defconfig
 TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := proton
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-$(TARGET_KERNEL_CLANG_VERSION)
 
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
